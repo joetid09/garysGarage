deeb6dc14e42d16852e856275825bf43c3db6546
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-30 10:41:18 -0600

    all vehicles info setup

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace garysGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cessna myCessna = new Cessna
+            {
+                MainColor = "red"
+            };
+
+            Console.WriteLine($"{myCessna.MainColor}");
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,5 @@
+public class Vehicle
+{
+    public string MainColor { get; set; }
+    public string MaximumOccupancy { get; set; }
+}
